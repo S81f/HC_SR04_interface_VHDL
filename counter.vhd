56ebd6bb09cceb8e344ea8fcb7c6acb966bec9b1
Saif Saifis <66900023+S81f@users.noreply.github.com>
2020-09-17 20:43:05 +0200

    Update counter.vhd
    Some final adjustments

--- counter.vhd
+++ counter.vhd
@@ -2,41 +2,25 @@
 -- Company: TEIS AB
 -- Engineer: Saif Saadaldin
 --
--- Create Date: 	 2020-05-30
--- Design Name: 	 HCSR04_sensor_interface
+-- Create Date:  2020-05-30
+-- Design Name:  HCSR04_sensor_interface
 -- Target Devices: ALTERA MAX 10. DE10-Lite board
 -- Tool versions:  Quartus Price Version 18.1.0
--- Testbench file: HCSR04_sensor_interface_vhd_tst.vht
--- Do file: 		 
+-- Testbench file: top_vhdl
+-- Do file: 	 wave.do
 --
 -- Description: This component will count the time the echo signal (echo_in pin) is high. The time
---		will be send in microseconds (us) to another component to measure the distance to an obsticle.
+--		 will be send in microseconds (us) to another component to measure the distance to an obsticle.
 --					 
---					 
---
--- In_signals:
--- 		clk_50		:	in	std_logic; (50 MHz)
---			reset_n		:	in	std_logic;	(Active low)
---			echo_in		:	in	std_logic;
---
---
---
--- Out_signals:
---		dv_n				:	out	std_logic; 	data valide (dv) will be high when sending pulse_time to other component.
---										In this way the next component will know that pulse_time value is right
---
---		pulse_time		:	out	std_logic_vector (23 downto 0);
 
-
--------------------------------------------------------------------
+---------------------------------------------------------------------------------------
 
 
 --**************************************************************************************************************************************'
--- if any obsticle is 400 cm away from sensor then it'll take the ultrasonic wave 1/850 sec to hit the obsticle.
--- and then another 1/850 sec to travel back to the sensor. So the max time the echo_in pulse needed to be high is equal 
--- to the total time it takes the ultrasonic wave to travel back and its 1/425 sec (1/850 + 1/850).
--- 1/425 sec give us 23529411.76 ns = approx 23,530,000.
---	To count that in FPGA with 50MHz clock-->20ns clock cycle = 25530000/20ns = 1176500 clock cycle is the nr of clock cycle when the eco is higt
+-- if any obsticle is 400 cm away from sensor and according to sensor datasheet then it'll take the ultrasonic wave 23200us to hit the obsticle
+-- and back to the sensor. 23,200 us = 232,00,000 ns
+
+-- To count that in FPGA with 50MHz clock-->20ns clock cycle = 23200000/20ns = 1160000 clock cycle is the nr of clock cycle when the eco is higt
 -- Thats why we need a std_logic_vector 23 downto 0 to our echo_pulse_counter 
 --*******************************************************************************************************************************************
 
@@ -45,20 +29,21 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+--use ieee.std_logic_arith.all;--fake library
 
 entity counter is
 
 		port
 		(
-			-- Input ports
-			clk_50		:	in	std_logic;
-			reset_n		:	in std_logic;
-			--echo_pulse	:	in	std_logic_vector (27 downto 0);
-			echo_in		:	in	std_logic;
-			
+			-- input ports
+			i_Clock			:	in std_logic;
+			i_Reset_n		:	in std_logic;
+			i_Echo			:	in std_logic;
 			-- Output ports
-			dv_n		:	out	std_logic;
-			pulse_time	:	out	std_logic_vector (23 downto 0)--kan de inte vara unsigned (27 downto 0)? jag fick fel när jag gjorde det!
+			o_DV_n			:	out std_logic;--data valide (dv) will be high when sending pulse_time to other component.
+--									In this way the next component will know that pulse_time value is right
+			o_Echo_pulse_time	:	out std_logic_vector(23 downto 0)
+	
 		);
 end counter;
 
@@ -66,7 +51,8 @@ end counter;
 
 architecture Behavioral of counter is
 
-signal reset_n_t1,reset_n_in				: std_logic;-- for metastability
+--signals has architecture scope
+signal reset_n_t1,reset_n_in			: std_logic;-- for metastability
 
 signal echo_pulse_t1,echo_pulse_t2, echo_pulse_in	: std_logic;-- for metastability
 
@@ -84,17 +70,20 @@ signal state: state_type;
 begin
 
 --***************************************************************************************************************************
-	reset_n_meta_stability:process(clk_50, reset_n)
+	
+--********************************************************************************************
+--It maybe useful to test reading input signal without meta stability to see what the results are
+--**********************************************************************************************
+	reset_n_meta_stability:process(i_Clock, i_Reset_n)
 	
 	
-		variable Reset_t2: std_logic;	--	variablar används för sekventiell exekvering. Variabel värde uppdatera på en gång, 
-												--	ingen delay till skillnad från signaler
-												--	variablar har lokal scope, inuti en process. Signalar har architecture scope
+		variable Reset_t2: std_logic;	--varibles are used in sequential VHDL inside a process. They are local inside the process and uptaded without any delay. 
+								
 			begin
 			
-				if rising_edge(clk_50) then
-					reset_n_t1 <= reset_n;
-					Reset_t2 := reset_n_t1;--värdet på Reset_t2 uppdateras direkt
+				if rising_edge(i_Clock) then
+					reset_n_t1 <= i_Reset_n;
+					Reset_t2 := reset_n_t1;
 					reset_n_in <= Reset_t2; 
 				end if;
 				
@@ -105,17 +94,15 @@ begin
 
 --***************************************************************************************************************************
 	
-	echo_pulse_meta_stability:process(clk_50, echo_in)
-	-- vad skiljer denna metastability process mot den ovan?
-
+	echo_pulse_meta_stability:process(i_Clock, i_Echo,reset_n_in)
 			begin
 			
 				if reset_n_in = '0' then
 					echo_pulse_t1 <= '0';
 					echo_pulse_t2 <= '0';
 					echo_pulse_in <= '0';
-				elsif rising_edge(clk_50) then
-					echo_pulse_t1 <= echo_in;
+				elsif rising_edge(i_Clock) then
+					echo_pulse_t1 <= i_Echo;
 					echo_pulse_t2 <= echo_pulse_t1;
 					echo_pulse_in <= echo_pulse_t2;
 				end if;
@@ -125,69 +112,63 @@ begin
 --***************************************************************************************************************************
 
 
-measuring_echo_pulse_time: process (clk_50,reset_n_in)
-begin
+measuring_echo_pulse_time: process (i_Clock,reset_n_in)
 
-	if (reset_n_in = '0') then
-		
-		state <= idle;
-		dv_n <= '1';--wont send data
-		
-
-	elsif (rising_edge(clk_50)) then 
-		
-		case state is
-				
-				when idle =>-- out of range (no echo
-				
-					echo_pulse_counter <= (others => '0');
-					dv_n <= '1';--wont send data
-					
-					if(echo_pulse_in = '1') then
-						state <= counting;
-					else
-						state <= idle; 	-- if there's no echo stay in idle or obsticle below 2cm (58823.52941ns)in distance
-					end if;
-					
-					
-				when counting =>
-					
-					echo_pulse_counter <= echo_pulse_counter + 1;--every +1 gives 20 ns
-								
-					
-					if(echo_pulse_in = '1') then
-						state <= counting;
-					else
-						state <= sending_info;
-
-					end if;
-					
-				--when conv_to_us =>
-					--vi kanske kan ha en tillstånd som omvandlar till us innan vi skickar till measuremeant component
-				
-					
-				when sending_info =>
-					
-					dv_n <= '0';
-					pulse_time <= std_logic_vector(echo_pulse_counter);
-					
-					if(echo_pulse_in = '1') then
-						state <= counting;
-					else
-						state <= idle;
-					end if;
-				
-				--when out_of_range =>
+	begin
+	
+		if (reset_n_in = '0') then
+			o_Echo_pulse_time<= (others => '0');
+			state <= idle;
+			o_DV_n <= '1';--wont send data
 			
+	
+		elsif (rising_edge(i_Clock)) then
 			
-				--when no_pos_change =>
+			case state is
+					
+					when idle => --out of range (no echo)
+					
+						echo_pulse_counter <= (others => '0');
+						o_DV_n <= '1';--wont send data
+						
+						if(echo_pulse_in = '1') then
+							state <= counting;
+						else
+							state <= idle; 	-- if there's no echo stay in idle or obsticle below 2cm (58823.52941ns)in distance
+						end if;
+						
+						
+					when counting =>
+						
+						echo_pulse_counter <= echo_pulse_counter + 1;--increases with 20ns every time									
+						
+						if(echo_pulse_in = '1') then
+							state <= counting;
+						else
+							state <= sending_info;
+						end if;
+									
+						
+					when sending_info =>
+						
+						o_DV_n <= '0';
+						o_Echo_pulse_time <= std_logic_vector(echo_pulse_counter);
+						
+						if(echo_pulse_in = '1') then
+							state <= counting;--echo_pulse_counter måste nollställas nånstans ju?
+						else
+							state <= idle;
+						end if;
+					
+					--when out_of_range =>
+						--maybe in future one can add these state
+				
+					--when no_pos_change =>
+						--maybe in future one can add these state
+			end case;
 			
-		end case;
-		
-		
-		
-	end if;
-end process measuring_echo_pulse_time;
+		end if;
+	end process measuring_echo_pulse_time;
 
 
 end Behavioral;
