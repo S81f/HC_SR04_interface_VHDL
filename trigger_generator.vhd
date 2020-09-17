240d65296deb78d8c4dbef970ae50cf093e99b2c
Saif Saifis <66900023+S81f@users.noreply.github.com>
2020-09-17 20:51:40 +0200

    Update trigger_generator.vhd

--- trigger_generator.vhd
+++ trigger_generator.vhd
@@ -1,31 +1,21 @@
 --------------------------------------------------------------------
--- Company:
--- Engineer: Saif 
+-- Company: TEIS AB
+-- Engineer: Saif Saadaldin
 --
 -- Create Date: 	 2020-05-30
 -- Design Name: 	 HCSR04_sensor_interface
--- Target Devices: 	ALTERA MAX 10. DE10-Lite board
--- Tool versions:  	Quartus Price Version 18.1.0
--- Testbench file: 	trigger_test.vht
--- Do file: 		trigger_test_run_msim_rtl_vhdl_saif.do
+-- Target Devices: ALTERA MAX 10. DE10-Lite board
+-- Tool versions:  Quartus Price Version 18.1.0
+-- Testbench file: top_vhdl
+-- Do file: 		 wave.do
 --
 -- Description:	Send a 10us trigger pulse to HCSRo4 sensor to activate the sensors distance measurement
---
--- In_signals:
--- 	clk_50		:	in	std_logic; (50 MHz)
---	reset_n		:	in	std_logic;	(Active low)
---
--- Out_signals:
--- 	trigger_pulse	:	out	std_logic; (10 us pulse)
 
-
+-- improvments:
+--***********************
+--Pulse generating can be done by integer, then it'll maybe easier to understand the code. Se teori 5b vhdl 1.
+--***********************
 -------------------------------------------------------------------
-
-
---************************************
---	det går att göra pulse genarating mha integer om det är enklare att förstå, se teori 5b vhdl 1
---*************************************
-
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -34,40 +24,38 @@ use ieee.numeric_std.all;
 
 entity trigger_generator is
 
-		port
-		(
-			-- Input ports
-			clk_50		:	in	std_logic;
-			reset_n		:	in	std_logic;
-	
-			-- Output ports
-			trigger_pulse	:	out	std_logic--one bit beacuse its only one input pin on the sensore side.
-		);
+	port
+	(
+
+		-- Input ports
+		i_Clock				:	in	std_logic;
+		i_Reset_n			:	in	std_logic;
+
+		-- Output ports
+		o_Trigger			:	out	std_logic	--one bit beacuse its only one input pin on the sensore side.
+	);
 end trigger_generator;
 
 
 
 architecture Behavioral of trigger_generator is
 
+
 signal counter 	: unsigned (23 downto 0) := "000000000000000000000000";-- counts up to 60ms
 
 
 begin
 
-process(clk_50)
+process(i_Clock,i_Reset_n)
 
 	begin
-	
-		if reset_n = '0' then
-			-- clear counter 
+
+		if i_Reset_n = '0' then
+			-- clear counter
 			counter <= (others => '0');
-			-- bör inte trigger_pulse var stabil i några ns efter en omstart?
-			--trigger_pulse <= (others => '0');--kan man göra så?
-			
-			--det kan bli bättre lösning om vi kan stoppa och nolla counter när en echo fås tillbaka.
-			--kanske går att göra  om man delar kodnigen i tillståndsmaskiner som i counter fallet
-		elsif rising_edge(clk_50) then
-			if(counter >= 3000000) then--3000000 motsvarar 60 ms
+
+		elsif rising_edge(i_Clock) then
+			if(counter >= 3000000) then--3000000 = 60ms. According to sensor datasheet it must by at least 60ms between each trigger signal 
 				counter <= (others => '0');
 			else
 				counter <= counter + 1;
@@ -79,9 +67,7 @@ end process;
 
 
 
-trigger_pulse <= '1' when counter >= 1 and counter <= 500 else '0';--under 10uS tid
-
---vänta 50ms eller få signal från counter på att du kan skicka trigger när inget echo finns
+o_Trigger <= '1' when counter >= 1 and counter <= 500 else '0';-- 10uS trigger signal
 
 end Behavioral;
 
